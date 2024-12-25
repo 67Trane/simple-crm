20787b94f4a74ed20392b49a091e9eb96d759c2c
Trane <Trane_67@hotmail.de>
2024-12-25 20:50:50 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButtonModule, MatSidenavModule, CommonModule],
+  imports: [MatButtonModule, MatSidenavModule, CommonModule, MatToolbarModule,MatIconModule, RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  showFiller = false;
+
 }
